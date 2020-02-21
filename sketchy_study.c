ef8898a5515bfb061cc3f789abaed64f34c105c1
Jeffrey Huang <jeffreyhuangdragon@gmail.com>
2020-02-21 01:25:45 -0500

    working on thread

--- sketchy_study.c
+++ sketchy_study.c
@@ -8,6 +8,18 @@
 #include <stdlib.h>
 #include "sketchy_study.h"
 
+#define NAME1 1
+#define NAME2 2
+#define NAME3 3
+#define NAME4 4
+#define RUNS 5
+
+int stn[4], stn_res[4];
+
+pthread_cond_t stn_1, stn_2, stn_3, stn_4, stn_1_res, stn_2_res, stn_3_res, stn_4_res;
+
+pthread_mutex_t grad_mutex;
+
 int main() {
 	struct args* grads_args[NUM_GRADS];
 	pthread_t grads[NUM_GRADS];
@@ -35,7 +47,7 @@ int* make_ordered_list() {
 	int i = NUM_GRADS - 1;
 	int j = 0;
 	int temp = 0;
-	int ordered_list[4] = {1, 2, 3, 4};
+	int ordered_list[4] = {0, 1, 2, 3};
 	int *a = ordered_list;
 
 	for (; i > 0; i--) {
@@ -50,4 +62,56 @@ int* make_ordered_list() {
 void* thread() {
 	printf("in thread\n");
 
+	args* instr;
+	int instr_cnt;
+	int instr_cnt_max;
+	int stn_sts; //station status
+	int stn_res_sts; //station reservation status
+	int r;
+
+	for(r=0;r<RUNS;r++){
+		instr = make_ordered_list();
+		instr_cnt = 0;
+		instr_cnt_max = instr->list_size;
+		
+
+		while(instr_cnt<instr_cnt_max){
+			stn_sts = stn[instr->ordered_list[instr_cnt]];
+
+			pthread_mutex_lock(&grad_mutex);
+			while(stn_sts!=0){
+				if(stn_sts==1) pthread_cond_wait(&stat_1, &grad_mutex);
+				else if(stn_sts==2) pthread_cond_wait(&stn_2, &grad_mutex);
+				else if(stn_sts==3) pthread_cond_wait(&stn_3, &grad_mutex);
+				else if(stn_sts==4) pthread_cond_wait(&stn_4, &grad_mutex);
+			}
+			while(stn_sts_res!=0){
+				if(stn_sts==1) pthread_cond_wait(&stat_1, &grad_mutex);
+				else if(stn_sts==2) pthread_cond_wait(&stn_2_res, &grad_mutex);
+				else if(stn_sts==3) pthread_cond_wait(&stn_3_res, &grad_mutex);
+				else if(stn_sts==4) pthread_cond_wait(&stn_4_res, &grad_mutex);
+			}
+			if(instr_cnt+1<instr_cnt_max){
+				stn_res_sts = stn_res[instr->ordered_list[instr_cnt+1]];
+				while(stn_sts!=0){
+					if(stn_sts==1) pthread_cond_wait(&stat_1_res, &grad_mutex);
+					else if(stn_sts==2) pthread_cond_wait(&stn_2, &grad_mutex);
+					else if(stn_sts==3) pthread_cond_wait(&stn_3, &grad_mutex);
+					else if(stn_sts==4) pthread_cond_wait(&stn_4, &grad_mutex);
+				}
+				while(stn_sts!=0){
+					if(stn_sts==1) pthread_cond_wait(&stat_1, &grad_mutex);
+					else if(stn_sts==2) pthread_cond_wait(&stn_2_res, &grad_mutex);
+					else if(stn_sts==3) pthread_cond_wait(&stn_3_res, &grad_mutex);
+					else if(stn_sts==4) pthread_cond_wait(&stn_4_res, &grad_mutex);
+				}
+			}
+
+			pthread_mutex_unlock(&grad_mutex);
+
+			sleep(1);
+
+			instr_cnt++;
+		}
+	}
 }
